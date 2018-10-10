b22a45965d1db15a06bb93fd43515e600a73db13
chengshunzhang <chengshun.zhang@outlook.com>
2018-10-09 18:07:47 -0700

    Solution to 157_Read_N_Characters_Given_Read4

--- 157_Read_N_Characters_Given_Read4/157_Read_N_Characters_Given_Read4.cpp
+++ 157_Read_N_Characters_Given_Read4/157_Read_N_Characters_Given_Read4.cpp
@@ -1,14 +1,27 @@
-#include <iostream>
-using namespace std;
+// class Solution {
+// public:
+//     /**
+//      * @param buf Destination buffer
+//      * @param n   Maximum number of characters to read
+//      * @return    The number of characters read
+//      */
+//     int read(char *buf, int n) {
+//         int offset = 4, num = read4(buf), result = num;
+//         while(result < n && num == 4) {
+//             num = read4(buf + offset);
+//             offset += 4;
+//             result += num;
+//         }
+//         if(result >= n) {
+//             return n;
+//         } else {
+//             return result;
+//         }
+//     }
+// };
 
-//Forward declaration of the read4 API.
-int read4(char *buf) {
-    int n = 0;
-    for(int i = 0; i < 4 && buf[i] != '\0'; i++) {
-        n++;
-    }
-    return n;
-};
+// Forward declaration of the read4 API.
+int read4(char *buf);
 
 class Solution {
 public:
@@ -18,24 +31,25 @@ public:
      * @return    The number of characters read
      */
     int read(char *buf, int n) {
-        int offset = 4, num = read4(buf), result = num;
+        char temp[4];
+        int i = 0, num = read4(temp), result = num;
         while(result < n && num == 4) {
-            num = read4(buf + offset);
-            offset += 4;
+            for(int j = 0; j < 4; j++) {
+                buf[i++] = temp[j];
+            }
+            num = read4(temp);
             result += num;
         }
         if(result >= n) {
+            for(int j = 0; j < n - result + num; j++) {
+                buf[i++] = temp[j];
+            }
             return n;
         } else {
+            for(int j = 0; j < num; j++) {
+                buf[i++] = temp[j];
+            }
             return result;
         }
     }
 };
-
-int main() {
-    Solution s;
-    char buf[] = "abc";
-    int n = 4;
-    cout << s.read(buf, n) << endl;
-    return 0;
-}
