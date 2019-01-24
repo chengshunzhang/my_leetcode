0401cda088bd61ccc447ee06da6ac64087616f16
chengshunzhang <chengshun.zhang@outlook.com>
2019-01-23 20:36:08 -0800

    Solution to 121_Best_Time_to_Buy_and_Sell_Stock

--- 101-200/121_Best_Time_to_Buy_and_Sell_Stock/121_Best_Time_to_Buy_and_Sell_Stock.cpp
+++ 101-200/121_Best_Time_to_Buy_and_Sell_Stock/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -5,34 +5,25 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        if(n == 0 || n == 1) {
+        if(prices.empty() || prices.size() == 1) {
             return 0;
         }
-        int A[n], min;
-        A[0] = 0;
-        int temp = prices[1] - prices[0];
-        if(temp > 0) {
-            A[1] = temp;
-            min = prices[0];
-        } else {
-            A[1] = 0;
-            min = prices[1];
-        }
-        for(int i = 2; i < n; i++) {
-            temp = prices[i] - min;
-            A[i] = temp > A[i - 1] ? temp : A[i - 1];
+        int min = INT_MAX, maxp = 0;
+        for(int i = 0; i < prices.size(); i++) {
             if(prices[i] < min) {
                 min = prices[i];
+            } else {
+                if(prices[i] - min > maxp) {
+                    maxp = prices[i] - min;
+                }
             }
         }
-        return A[n - 1];
+        return maxp;
     }
 };
 
 int main() {
-	int n[] = {7,1,5,3,6,4};
-	vector<int> prices(n, n + 6);
+	vector<int> prices = {7,1,5,3,6,4};
 	Solution s;
 	cout << s.maxProfit(prices) << endl;
 	return 0;
