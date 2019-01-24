bed382bd3fa749aa8a4c4b9593e72322f6be99aa
chengshunzhang <chengshun.zhang@outlook.com>
2019-01-23 22:21:17 -0800

    Solution to 122_Best_Time_to_Buy_and_Sell_Stock_II

--- 101-200/122_Best_Time_to_Buy_and_Sell_Stock_II/122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ 101-200/122_Best_Time_to_Buy_and_Sell_Stock_II/122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -5,20 +5,14 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0, i = 1, n = prices.size();
-        while(i < n) {
-            while(i < n && prices[i] <= prices[i - 1]) {
+        int profit = 0;
+        for(int i = 0; i + 1 < prices.size(); i++) {
+            int start = i;
+            while(i + 1 < prices.size() && prices[i] < prices[i + 1]) {
                 i++;
             }
-            if(i == n) {
-                break;
-            } else {
-                int buy = i - 1;
-                while(i < n && prices[i] >= prices[i - 1]) {
-                    i++;
-                }
-                int sell = i - 1;
-                profit += prices[sell] - prices[buy];
+            if(i != start) {
+                profit += prices[i] - prices[start];
             }
         }
         return profit;
@@ -26,8 +20,7 @@ public:
 };
 
 int main() {
-	int n[] = {7,1,5,3,6,4};
-	vector<int> prices(n, n + 6);
+	vector<int> prices = {7,1,5,3,6,4};
 	Solution s;
 	cout << s.maxProfit(prices) << endl;
 	return 0;
---
+++ 101-200/122_Best_Time_to_Buy_and_Sell_Stock_II/README.md
@@ -0,0 +1,2 @@
+Find all the increasing sequences of prices array. For each interval, add  
+`prices[end] - prices[start]` to profit.
