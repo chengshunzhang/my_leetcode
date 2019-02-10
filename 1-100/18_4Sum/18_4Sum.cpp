5a0fa29f53d1dbade12565174ffe3ee9914dabc0
chengshunzhang <chengshun.zhang@outlook.com>
2019-02-10 14:09:07 -0800

    Solution to 18_4Sum

--- 1-100/18_4Sum/18_4Sum.cpp
+++ 1-100/18_4Sum/18_4Sum.cpp
@@ -4,54 +4,54 @@ using namespace std;
 
 class Solution {
 public:
-	vector< vector<int> > fourSum(vector<int>& nums, int target) {
-		vector< vector<int> > result;
-		if(nums.size() < 4)
-			return result;
+	vector<vector<int>> fourSum(vector<int>& nums, int target) {
+		vector<vector<int>> result;
+        int n = nums.size();
+        if(n < 4) {
+            return result;
+        } 
 		sort(nums.begin(), nums.end());
-		int i = 0;
-		while(i < nums.size() - 3) {
-			int j = i + 1, target3 = target - nums[i];
-			while(j < nums.size() - 2) {
-				int num = target3 - nums[j];
-				int left = j + 1, right = nums.size() - 1;
-				while (left < right) {
-					if(nums[left] + nums[right] == num) {
-						vector<int> temp;
-						temp.push_back(nums[i]); temp.push_back(nums[j]); temp.push_back(nums[left]); temp.push_back(nums[right]);
-						result.push_back(temp);
-						left++;
-						while(nums[left] == nums[left - 1])
-							left++;
-						right--;
-						while(nums[right] == nums[right + 1])
-							right--;
-					}
-					else if(nums[left] + nums[right] > num)
-						right--;
-					else
-						left++;
-				}
-				j++;
-				while(nums[j] == nums[j - 1])
-					j++;
-			}
-			i++;
-			while(nums[i] == nums[i - 1])
-				i++;
-		}
+        for(int i = 0; i + 3 < n; i++) {
+            for(int j = i + 1; j + 2 < n; j++) {
+                int targetSum = target - nums[i] - nums[j], left = j + 1, right = n - 1;
+                while(left < right) {
+                    int sum = nums[left] + nums[right];
+                    if(sum == targetSum) {
+                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        left++;
+                        right--;
+                        while(left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while(left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if(sum < targetSum) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+                while(j + 2 < n && nums[j] == nums[j + 1]) {
+                    j++;
+                }
+            }
+            while(i + 3 < n && nums[i] == nums[i + 1]) {
+                i++;
+            }
+        }
 		return result;
 	}
 };
 
 int main(){
-	int n[] = {1, 0, -1, 0, -2, 2}, target = 0;
-	vector<int> nums(n,n+6);
+	vector<int> nums = {1, 0, -1, 0, -2, 2}, target = 0;
 	Solution s;
 	vector< vector<int> > res = s.fourSum(nums, target);
 	for (int i = 0; i < res.size(); i++) {
-		for (int j = 0; j < 4; j++)
+		for (int j = 0; j < 4; j++) {
 			cout << res[i][j] << ' ';
+		}
 		cout << endl;
 	}
 	return 0;
---
+++ 1-100/18_4Sum/README.md
@@ -0,0 +1,3 @@
+### Two pointers  
+Basically the same as 3sum, just adding one more outer loop.  
+One thing to notice is that in order to remove duplicates, when the sum is target, increase left pointer until get a new value and decrease right pointer until get a new value. And at the end of each outer loop (i and j), increase until get new values.
