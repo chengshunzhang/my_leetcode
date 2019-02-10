91e95bde3e7455e05aed566905b06749e6135e2c
chengshunzhang <chengshun.zhang@outlook.com>
2019-02-10 13:42:06 -0800

    Solution to 11_Container_With_Most_Water

--- 1-100/11_Container_With_Most_Water/11_Container_With_Most_Water.cpp
+++ 1-100/11_Container_With_Most_Water/11_Container_With_Most_Water.cpp
@@ -6,23 +6,24 @@ class Solution {
 public:
 	int maxArea(vector<int>& height) {
 		int left = 0, right = height.size() - 1;
-		int max = (height[left] > height[right]) ? height[right] * (right - left) : height[left] * (right - left);
-		while (left < right) {
-			if (height[left] < height[right])
-				left++;
-			else
-				right--;
-			int temp = (height[left] > height[right]) ? height[right] * (right - left) : height[left] * (right - left);
-			if (temp > max)
-				max = temp;
+		int max = min(height[left], height[right]) * (right - left);
+		while(left < right) {
+			if(height[left] < height[right]) {
+                left++;
+            } else {
+               right--; 
+            }
+			int temp = min(height[left], height[right]) * (right - left);
+			if(temp > max) {
+                max = temp;
+            }	
 		}
-		return max;
-	}
+        return max;
+    }
 };
 
 int main(){
-	int h[] = {6,4,3,1,4,6,99,62,1,2,6};
-    vector<int> height(h,h+11);
+	vector<int> height = {6,4,3,1,4,6,99,62,1,2,6};
     Solution s;
     cout << s.maxArea(height) << endl;
 	return 0;
---
+++ 1-100/11_Container_With_Most_Water/README.md
@@ -0,0 +1,3 @@
+### Two pointers  
+Left points to first element and right points to last element. Calculate the area.  
+While left is smaller than right, move the smaller pointer and recalculate the area. If larger than the current maximum, update.
